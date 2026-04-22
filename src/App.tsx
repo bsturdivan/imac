c3f7841df10f5f2ec170f52b133d029a95fba5d9
Brian Sturdivan <bsturdivan@gmail.com>
2026-04-22 14:06:33 -0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <div className='computer'>
+        <div className='screen-frame'></div>
+        <div className='screen'></div>
+
+        <div className='utility-parent'>
+          <div className='utility-container'>
+            <div className='utility-tab'></div>
+            <div className='utility-tab'></div>
+
+            <div className='utilities'>
+              <div className='cdr'></div>
+              <div className='headphone-back'></div>
+              <button className='onoff' type='button'></button>
+            </div>
+
+            <div className='speakers'>
+              <div className='speaker'>
+                <div className='cone'></div>
+              </div>
+              <div className='speaker'>
+                <div className='cone'></div>
+              </div>
+            </div>
+          </div>
+
+          <div className='utility-container-top'>
+            {/* <div className='utilities'>
+              <div className='cdr'></div>
+              <div className='headphone-back'></div>
+              <button className='onoff' type='button'></button>
+            </div> */}
+
+            <div className='speakers'>
+              <div className='speaker dots'></div>
+              <div className='speaker dots'></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default App
