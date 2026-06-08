36c727c9e4d46e41c855b336efdf21e69d05cc43
Brian Sturdivan <bsturdivan@gmail.com>
2026-06-08 16:06:27 -0700

    smarter timing buffer

--- src/Intro/Line.tsx
+++ src/Intro/Line.tsx
@@ -1,8 +1,6 @@
 import Input from '../Terminal/Input'
 import { useState, useEffect, useRef } from 'react'
 
-const BUFFER = 4 // time to pause after each line, measured in character.
-
 function randomNumber(min: number, max: number) {
   return Math.random() * (max - min) + min
 }
@@ -26,19 +24,15 @@ function Line({
   const doneRef = useRef(false)
 
   useEffect(() => {
-    if (currentIndex >= text.length + BUFFER) {
-      console.log('currentIndex >= text.length + BUFFER', text[currentIndex])
+    if (currentIndex >= text.length) {
       if (!doneRef.current && index + 1 !== allText.length) {
-        console.log('doneRef.current = true', text[currentIndex])
         doneRef.current = true
         setRenderedLines((prevText: string[]) => [...prevText, allText[index + 1]])
       }
       return
     }
 
-    const delay = currentIndex === 0 ? 666 : randomNumber(70, 110)
-    console.log('TIMEOUT', currentIndex, text[currentIndex])
-
+    const delay = currentIndex === 0 ? 666 : randomNumber(70, 120)
     const timeout = setTimeout(() => setCurrentIndex((prev) => prev + 1), delay)
 
     return () => clearTimeout(timeout)
