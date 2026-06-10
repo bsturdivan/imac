9bea5c77a3820ac45a4874cd19338d10a5429a8e
Brian Sturdivan <bsturdivan@gmail.com>
2026-06-09 22:46:14 -0700

    edit meta

--- src/main.tsx
+++ src/main.tsx
@@ -5,23 +5,8 @@ import './styles/fonts.css'
 import './styles/base.css'
 import App from './App.tsx'
 
-const year = new Date().getFullYear()
-
-const description = `Brian Sturdivan
-San Francisco, ${year}\n
-The Illusion of Convenience, 1999
-HTML and CSS on screen.`
-
-const title = 'Brian Sturdivan | The Illusion of Convenience'
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <meta name='description' content={description} />
-    <title>{title}</title>
-    <meta property='og:title' content={title} />
-    <meta property='og:description' content={description} />
-    <meta property='og:image' content='/og.png' />
-    <meta property='og:url' content={window.location.href} />
     <App />
   </StrictMode>,
 )
